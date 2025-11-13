cdafec9c9d7a0b4d886abe1801263ea53f078a2b
Uthman <68771485+omenarism@users.noreply.github.com>
2025-11-13 20:22:54 +0800

    add human and computer score

--- script.js
+++ script.js
@@ -12,37 +12,59 @@ function translateToItem(compA) {
     }
 }
 
+
 function getHumanChoice() {
     return prompt("Rock, Paper, or Scissors: ");
 }
+
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if(humanChoice == 'rock' && computerChoice == 'rock'){
         console.log('A TIE');
     } else if (humanChoice == 'rock' && computerChoice == 'paper') {
         console.log('You Lose! Paper beats Rock');
+        computerScore++;
     } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
         console.log('You Win! Rock beats Scissors');
+        humanScore++;
     } else if (humanChoice == 'paper' && computerChoice == 'rock') {
         console.log('You Win! Paper beats Rock');
+        humanScore++;
     }  else if (humanChoice == 'paper' && computerChoice == 'paper') {
         console.log('A Tie');
     }  else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         console.log('You Lose! Scissors beats Paper');
+        computerScore;
     }  else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         console.log('You Lose! Scissors beats Rock');
+        computerScore++;
     }  else if (humanChoice == 'scissors' && computerChoice == 'paper') {
         console.log('You Win! Scissors beats Paper');
+        humanScore++;
     }  else {
         console.log('A Tie');
     }   
 }
 
-const humanSelection = getHumanChoice();
+function playGame(){
+
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
 const computerSelection = translateToItem(getComputerChoice());
 console.log('You has chosen: ' + humanSelection);
 console.log('Computer has chosen: ' + computerSelection);
 playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+
+// playGame(playRound());
+console.log('human score: ' + humanScore);
+console.log('computer score: ' + computerScore);
 
 
 
