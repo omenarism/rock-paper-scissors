1bbc3d13322ebc2e9502c9b5084a584f861a1146
Uthman <68771485+omenarism@users.noreply.github.com>
2025-11-14 10:41:50 +0800

    finishing touch 5times repeat w/o loops

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function translateToItem(compA) {
     }
 }
 
-
 function getHumanChoice() {
     return prompt("Rock, Paper, or Scissors: ");
 }
@@ -32,39 +31,56 @@ function playRound(humanChoice, computerChoice) {
     } else if (humanChoice == 'paper' && computerChoice == 'rock') {
         console.log('You Win! Paper beats Rock');
         humanScore++;
-    }  else if (humanChoice == 'paper' && computerChoice == 'paper') {
-        console.log('A Tie');
-    }  else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+    } else if (humanChoice == 'paper' && computerChoice == 'paper') {
+        console.log('A TIE!');
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         console.log('You Lose! Scissors beats Paper');
-        computerScore;
-    }  else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        computerScore++;
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         console.log('You Lose! Scissors beats Rock');
         computerScore++;
-    }  else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
         console.log('You Win! Scissors beats Paper');
         humanScore++;
-    }  else {
-        console.log('A Tie');
+    } else {
+        console.log('A TIE!');
     }   
 }
 
-function playGame(){
-
+function playGame(humanSelection, computerSelection, humanSelection, computerSelection){
+    humanSelection = getHumanChoice().toLowerCase();
+    computerSelection = translateToItem(getComputerChoice());
+    console.log('You has chosen: ' + humanSelection);
+    console.log('Computer has chosen: ' + computerSelection);
+    playRound(humanSelection, computerSelection);
+    console.log('Human score: ' + humanScore + ' || Computer score: ' + computerScore);
 }
 
-const humanSelection = getHumanChoice().toLowerCase();
-const computerSelection = translateToItem(getComputerChoice());
-console.log('You has chosen: ' + humanSelection);
-console.log('Computer has chosen: ' + computerSelection);
-playRound(humanSelection, computerSelection);
-playRound(humanSelection, computerSelection);
-playRound(humanSelection, computerSelection);
-playRound(humanSelection, computerSelection);
-playRound(humanSelection, computerSelection);
+const humanSelection = "";
+const computerSelection = "";
 
-// playGame(playRound());
-console.log('human score: ' + humanScore);
-console.log('computer score: ' + computerScore);
+// humanSelection = getHumanChoice().toLowerCase();
+// computerSelection = translateToItem(getComputerChoice());
+
+
+
+playGame(humanSelection, computerSelection);
+playGame(humanSelection, computerSelection);
+playGame(humanSelection, computerSelection);
+playGame(humanSelection, computerSelection);
+playGame(humanSelection, computerSelection);
+
+const humanOtpt = document.getElementById("human-score");
+humanOtpt.style.backgroundColor = "#333";
+humanOtpt.style.color = "#ccc";
+humanOtpt.innerHTML = "THE SCORE OF HUMAN IS: " + humanScore;
+
+const computerOtpt = document.getElementById("computer-score");
+computerOtpt.style.backgroundColor = "#333";
+computerOtpt.style.color = "#ccc";
+computerOtpt.innerHTML = "THE SCORE OF COMPUTER IS: " + computerScore;
+
+
 
 
 
